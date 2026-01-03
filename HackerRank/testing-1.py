fd91f3d730bc91a37d659ba924cfcd868797ade4
yasiru kodikara <yasirukodikara15@gmail.com>
2026-01-03 06:05:51 +0530

    HR-1-2

--- HackerRank/testing-1.py
+++ HackerRank/testing-1.py
@@ -1,20 +1,15 @@
-def swap_case(s):
-    x = list(s)
-    rec = []
-    for i in x:
-
-        if i.islower():
-            f = i.upper()
-
-            rec.append(f)
-        else:
-            f = i.lower()
-            rec.append(f)
-    string = ''.join(rec)
-    return string
+def bubbleSort(arr):
+    n = len(arr)
+    for k in range(0,n):
+        for j in range(0,n-k-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
 
 
-if __name__ == '__main__':
-    s = input()
-    result = swap_case(s)
-    print(result)
+arr = [31415926535897932384626433832795, 1, 3, 10, 3, 5]
+
+print('before sorting ',arr)
+
+bubbleSort(arr)
+
+print('after sorting ',arr)
